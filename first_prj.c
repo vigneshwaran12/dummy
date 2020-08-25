26952afb52c0f26a8a2255441a5d9d003586d071
vigneshwaran <vigneshwaran.6363@gmail.com>
2020-08-25 08:38:45 -0700

    v2

---
+++ first_prj.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+#ifndef CHECKS
+	char *p = "testing";
+	printf("in if case...\n");
+	fflush(stdout);
+#else
+	char p[10] = { 0 };
+	printf("in else case...\n");
+	fflush(stdout);
+#endif
+	//modify readonly memory
+	*p = 'v';
+	printf("%s\n", p);
+	return 0;
+}
